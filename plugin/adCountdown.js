ad20bae99899c044f290f6ae0051de083fdba462
Matthew Boles <mboles@brightcove.com>
2017-08-21 16:42:55 -0400

    update

--- plugin/adCountdown.js
+++ plugin/adCountdown.js
@@ -1,4 +1,33 @@
 videojs.registerPlugin('adCountdown', function() {
+  // +++ Helpful method to convert seconds +++
+  /**
+   * Utility to extract h/m/s from seconds
+   * @param {number} secs - seconds to convert to hh:mm:ss
+   */
+  function secondsToTime(secs) {
+    var hours = Math.floor(secs / (60 * 60));
+    if (hours < 10) {
+      hours = "0" + hours.toString();
+    } else {
+      hours = hours.toString();
+    };
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+    minutes = minutes.toString();
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+    if (seconds < 10) {
+      seconds = "0" + seconds.toString();
+    } else {
+      seconds = seconds.toString();
+    };
+    var obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }  
   // Start code for plugin
   var myPlayer = this;
   // Initially  hide the overlay
@@ -30,33 +59,4 @@ videojs.registerPlugin('adCountdown', function() {
       document.getElementById('timeTarget').innerHTML = '';
     });
   });
-  // +++ Helpful method to convert seconds +++
-  /**
-   * Utility to extract h/m/s from seconds
-   * @param {number} secs - seconds to convert to hh:mm:ss
-   */
-  function secondsToTime(secs) {
-    var hours = Math.floor(secs / (60 * 60));
-    if (hours < 10) {
-      hours = "0" + hours.toString();
-    } else {
-      hours = hours.toString();
-    };
-    var divisor_for_minutes = secs % (60 * 60);
-    var minutes = Math.floor(divisor_for_minutes / 60);
-    minutes = minutes.toString();
-    var divisor_for_seconds = divisor_for_minutes % 60;
-    var seconds = Math.ceil(divisor_for_seconds);
-    if (seconds < 10) {
-      seconds = "0" + seconds.toString();
-    } else {
-      seconds = seconds.toString();
-    };
-    var obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
-  }
 });
